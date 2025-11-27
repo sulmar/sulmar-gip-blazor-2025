9603afb68edbe61095d272d339d879f9564f318e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-27 12:52:31 +0100

    Zastosowanie partial class

---
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+
+namespace BlazorWebAssemblyApp.Pages.Customers;
+
+public partial class List
+{
+    private string message = "Lorem ipsum...";
+
+    private List<Customer> customers = [];
+
+    private DateTime GetCurrentDate()
+    {
+        return DateTime.Now;
+    }
+
+    protected override void OnInitialized()
+    {
+        customers = new List<Customer>
+        {
+            new Customer { Id =1, Name = "a", Email = "a@domain.com"},
+            new Customer { Id =2, Name = "b", Email = "b@domain.com"},
+            new Customer { Id =3, Name = "c", Email = "c@domain.com"},
+        };
+    }
+}
