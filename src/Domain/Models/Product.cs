1bf537d553f99ae56e20eceddb93646f11e20c3a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-27 12:44:48 +0100

    Zamiana listy na tabelę

--- src/Domain/Models/Product.cs
+++ src/Domain/Models/Product.cs
@@ -4,4 +4,6 @@ public class Product : BaseEntity
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Color { get; set; }
+    public decimal UnitPrice { get; set; }
 }
