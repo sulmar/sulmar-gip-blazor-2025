efe87aceea4289287d4ca1dea986cba4536dcfc6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-27 12:18:39 +0100

    Dodanie listy klientow

---
+++ src/Domain/Models/Customer.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models;
+
+public class Customer : BaseEntity
+{    
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
---
+++ src/Domain/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
