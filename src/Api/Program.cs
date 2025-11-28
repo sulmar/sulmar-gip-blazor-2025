f70172ca851144f59329ccd940b4031d02b10bda
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-28 11:40:09 +0100

    Dodanie pobrania klientow z api

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -6,7 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICustomerRepository, FakeCustomerRepository>();
 builder.Services.AddScoped<SqlConnection>(sp => new SqlConnection(builder.Configuration.GetConnectionString("MyConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+     //   policy.WithOrigins("https://localhost:7283");
+        policy.AllowAnyOrigin();        
+        policy.WithMethods("GET");
+        // policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors(); // Middleware
 
 // Minimal Api
 
