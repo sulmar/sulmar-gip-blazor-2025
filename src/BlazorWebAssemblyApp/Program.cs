496ea3a754acdd0c5622a8752958d7fd17f0c6bf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-28 12:33:35 +0100

    Dodanie wstrzykiwania HttpClient do uslugi

--- src/BlazorWebAssemblyApp/Program.cs
+++ src/BlazorWebAssemblyApp/Program.cs
@@ -1,12 +1,16 @@
+using BlazorWebAssemblyApp;
+using BlazorWebAssemblyApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorWebAssemblyApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7247") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IAsyncCustomerService, ApiCustomerService>(
+    http => http.BaseAddress = new Uri("https://localhost:7247") );
+
 
 await builder.Build().RunAsync();
---
+++ src/BlazorWebAssemblyApp/Services/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System.Net.Http.Json;
+
+namespace BlazorWebAssemblyApp.Services;
+
+public interface IAsyncCustomerService
+{
+    Task<List<Customer>?> GetActive();
+    Task<List<Customer>?> GetArchive();
+}
+
+
+// Primary Constructor
+public class ApiCustomerService(HttpClient http): IAsyncCustomerService
+{
+    public Task<List<Customer>?> GetActive() => http.GetFromJsonAsync<List<Customer>>("api/customers");
+    public Task<List<Customer>?> GetArchive() => http.GetFromJsonAsync<List<Customer>>("api/customers/archive");
+}
