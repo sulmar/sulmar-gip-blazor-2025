d037b401e9f6a3020cd87f4d7ad58c27f1e4e2ba
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-10 14:49:43 +0100

    Dostowanie nazwy pola

--- src/Domain/Models/Validators/CustomerValidator.cs
+++ src/Domain/Models/Validators/CustomerValidator.cs
@@ -7,7 +7,7 @@ public class CustomerValidator : AbstractValidator<Customer>
 {
     public CustomerValidator()
     {
-        RuleFor(p => p.Name).NotEmpty().Length(3, 10);
+        RuleFor(p => p.Name).NotEmpty().Length(3, 10).WithName("Nazwa");
         RuleFor(p => p.Email).EmailAddress();
         RuleFor(p => p.Code).Matches(@"^\d{3}-[a-z|A-Z]{3}$").WithMessage("Podaj w formacie NNN-XXX");
         RuleFor(p => p.Salary).InclusiveBetween(100, 500);
