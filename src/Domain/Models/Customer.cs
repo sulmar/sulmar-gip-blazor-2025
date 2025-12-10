2e5d0bdb4151cf7f5c1951333fc9a3dd22a2710a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-10 14:47:57 +0100

    Dodanie FluentValidation

--- src/Domain/Models/Customer.cs
+++ src/Domain/Models/Customer.cs
@@ -4,20 +4,13 @@ namespace Domain.Models;
 
 public class Customer : BaseEntity
 {
-    [Required, StringLength(maximumLength: 10, MinimumLength = 3)]
     public string Name { get; set; }
-    [EmailAddress]
     public string Email { get; set; }
     public Region? Region { get; set; }
-    [RegularExpression(@"^\d{3}-[a-z|A-Z]{3}$", ErrorMessage = "Podaj w formacie NNN-XXX")]
     public string Code { get; set; }
-    public DateTime? Birthday { get; set; }
-    [Range(minimum: 100, maximum: 500)]
+    public DateTime? Birthday { get; set; }    
     public decimal Salary { get; set; }
     public bool IsArchived { get; set; }
-
     public string Password { get; set; }
-
-    [Compare(nameof(Password))]
     public string ConfirmPassword { get; set; }
 }
---
+++ src/Domain/Models/Validators/CustomerValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Domain.Models.Validators;
+
+// dotnet add package FluentValidation
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().Length(3, 10);
+        RuleFor(p => p.Email).EmailAddress();
+        RuleFor(p => p.Code).Matches(@"^\d{3}-[a-z|A-Z]{3}$").WithMessage("Podaj w formacie NNN-XXX");
+        RuleFor(p => p.Salary).InclusiveBetween(100, 500);
+        RuleFor(p => p.ConfirmPassword).Equal(p => p.Password);
+    }
+}
