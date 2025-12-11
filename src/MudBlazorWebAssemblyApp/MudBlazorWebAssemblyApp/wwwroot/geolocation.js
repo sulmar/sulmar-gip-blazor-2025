660b60080df4fe7b11bf0a7dd48e56e6e2b445d6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-11 15:30:41 +0100

    Pobieranie lokalizacji

---
+++ src/MudBlazorWebAssemblyApp/MudBlazorWebAssemblyApp/wwwroot/geolocation.js
@@ -0,0 +1,10 @@
+﻿window.getLocation = (dotnet) => {
+
+    navigator.geolocation.getCurrentPosition(position => {
+
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        
+        dotnet.invokeMethodAsync("OnLocationChanged", position.coords.latitude, position.coords.longitude);
+    });
+};
